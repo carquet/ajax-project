0442fb8c2a4c2787d9b2085ca4c14e71832729b4
L.Carquet <laetitiacarquet@gmail.com>
2017-01-08 13:07:48 +0100

    extract NYTime article according to city and iterate over the response

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -16,15 +16,34 @@ function loadData() {
     
     // YOUR CODE GOES HERE!
     var $street = $('#street').val();
-    console.log($street);
     var $city = $('#city').val();
-    console.log($city);
     var address = $street + ", " + $city;
-    console.log(address)
-    var addressUrl = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=" + address + "";
-    console.log(addressUrl)
-    $body.append('<img class="bgimg" src="'+ addressUrl + '">');
+    $greeting.text('So, you want to live at ' + address + '?');
+
+    // var addressUrl = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=" + address + "";
+    // $body.append('<img class="bgimg" src="'+ addressUrl + '">');
+    
+
+    var nyTimesUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + $city + '&sort=newest&api-key=4c810a6f5ef448ebad93168acccac947'
+    $.getJSON(nyTimesUrl, function(response) {
+            $nytHeaderElem.text('New York Times articles about ' + $city);
+            var data = response;
+            var articles = data.response.docs
+            articles.forEach(function(article){
+                console.log(article.headline.main)
+
+            })
+    })
+            
+
+
     return false;
+
+    
+ 
 };
+
+    
+
 
 $('#form-container').submit(loadData);
