7a8f3d4438e180e38a1744b8edad2ff77031c2ec
L.Carquet <laetitiacarquet@gmail.com>
2017-01-08 14:10:16 +0100

    add a second option to ajax call

--- ajax-project/js/script.js
+++ ajax-project/js/script.js
@@ -20,22 +20,46 @@ function loadData() {
     var address = $street + ", " + $city;
     $greeting.text('So, you want to live at ' + address + '?');
 
-    // var addressUrl = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=" + address + "";
-    // $body.append('<img class="bgimg" src="'+ addressUrl + '">');
+    var addressUrl = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location=" + address + "";
+    $body.append('<img class="bgimg" src="'+ addressUrl + '">');
     
 
     var nyTimesUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + $city + '&sort=newest&api-key=4c810a6f5ef448ebad93168acccac947'
+
+    // performance of getjson: 1.17s
     $.getJSON(nyTimesUrl, function(response) {
             $nytHeaderElem.text('New York Times articles about ' + $city);
             var data = response;
             var articles = data.response.docs
             articles.forEach(function(article){
-                console.log(article.headline.main)
+                console.log()
+                var htmlArticles = `<li class="article">`+ 
+                                        `<a href="`+ article.web_url +`">` + article.headline.main + `</a>` +
+                                        `<p>` + article.snippet + `</p>` +
+                                    `</li>`
 
+                $('#nytimes-articles').append(htmlArticles)
             })
     })
-            
-
+    
+    // performance of ajax : 1.67s    
+    // $.ajax({
+    //     datatype: "json",
+    //     url: nyTimesUrl,
+    //     success: function(response){
+    //         $nytHeaderElem.text('New York Times articles about ' + $city);
+    //         var data = response;
+    //         var articles = data.response.docs
+    //         articles.forEach(function(article){
+    //             console.log()
+    //             var htmlArticles = `<li class="article">`+ 
+    //                                     `<a href="`+ article.web_url +`">` + article.headline.main + `</a>` +
+    //                                     `<p>` + article.snippet + `</p>` +
+    //                                 `</li>`
+    //             $('#nytimes-articles').append(htmlArticles)
+    //         })
+    //     }
+    // })
 
     return false;
 
